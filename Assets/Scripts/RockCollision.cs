54c6ff454f2c369fa97abb39093c8a39a854454e
ssparky <stefan.vonk@gmail.com>
2023-09-21 15:20:06 +0200

    rock
    epic rock

---
+++ Assets/Scripts/RockCollision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player is currently not jumping and hits the rock, inflict 2 second stun?
+    }
+}
