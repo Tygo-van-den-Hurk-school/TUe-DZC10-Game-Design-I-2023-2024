ec1c6c77daf92ba6c11ac903b8dcf1ce60b1cc66
ssparky <stefan.vonk@gmail.com>
2023-10-25 12:01:06 +0200

    Fade-in/out
    Fade on title screen works, but cannot get fade in on gameplay screen to work. Looks okay enough without it though, so we can just proceed without it.

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -2,30 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
-    public string sceneToLoad;
-    Fade fade;
+    public GameObject fadeInCanvas;
+    public GameObject fader;              //duplicate which for some reason sort of causes things to work??
+
+    public Button startButton;
+
+    public FadeOut fadeout;
 
     void Start()
     {
-        fade = FindObjectOfType<Fade>();
+        fader.SetActive(false);
+        startButton.onClick.AddListener(LoadGameScene);
     }
-
-
 
     public void LoadGameScene()
     {
         // Load async to allow for fading in and out
-        StartCoroutine(LoadSceneAsync());
+        CanvasGroup canvasGroup = fader.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0.0f;
+        fader.SetActive(true); 
+        StartCoroutine(LoadSceneAsync(canvasGroup));
     }
 
-    private IEnumerator LoadSceneAsync()
+    private IEnumerator LoadSceneAsync(CanvasGroup canvasGroup)
     {
-        fade.FadeIn();
         yield return new WaitForSeconds(1.0f);
+        fader.SetActive(true); // failsafe
 
-        SceneManager.LoadScene(sceneToLoad);
+        float duration = 2.0f;
+        float startAlpha = 0.0f;
+        float endAlpha = 1.0f;
+        float timeElapsed = 0.0f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, timeElapsed / duration);
+            canvasGroup.alpha = alpha;
+
+            yield return null;
+        }
+        canvasGroup.alpha = endAlpha;
+        Time.timeScale = 1;
+
+        //fadeout.work = true;
+        SceneManager.LoadScene("DevelopmentScene");
     }
 }
