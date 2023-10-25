c1265d68960bdc5b53293f567e9a5b4d611d1bcb
Iliyan Teofilov <iliyanvt18@gmail.com>
2023-10-25 20:32:42 +0200

    Added sprites for level, and background

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private float parallaxEffectMultiplier;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position = new Vector2(transform.position.x + deltaMovement.x * parallaxEffectMultiplier, transform.position.y);
+        lastCameraPosition = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector2(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
+    }
+}
